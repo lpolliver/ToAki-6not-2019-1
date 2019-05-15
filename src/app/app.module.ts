4c6c483cd46e5ad72a2355556d06f5563d9c2d6b
Lucas Barcelos <lucasdesignerlt@gmail.com>
2019-05-14 21:18:09 -0300

    atualizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-import { MaterialModule } from './material/material.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-=======
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MaterialModule } from './material/material.module';
 import { MatSidenavModule, MatToolbarModule } from '@angular/material';
->>>>>>> e4066183b4536b05e1c7823399a9b621fda5f3c4
 
 @NgModule({
   declarations: [
@@ -26,11 +20,8 @@ import { MatSidenavModule, MatToolbarModule } from '@angular/material';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-<<<<<<< HEAD
-=======
     MatSidenavModule,
     MatToolbarModule,
->>>>>>> e4066183b4536b05e1c7823399a9b621fda5f3c4
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
